197139028d2722568217d1d0d559e6ca167545ec
EliasAachach <eliasaachach@gmail.com>
2022-03-09 13:54:29 +0100

    add constructors and destructor

--- includes/Vector.hpp
+++ includes/Vector.hpp
@@ -1,9 +1,10 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-# include <iostream>
-# include <string>
 # include <memory>
+# include <stdexcept>
+# include "Random_access_iterator.hpp"
+# include "Utils.hpp"
 
 namespace ft
 {
@@ -11,7 +12,23 @@ template < typename T, class Alloc = std::allocator<T> >
 	class Vector
 	{
 
+		private:
+
 		public:
+
+	/********************************/
+	/*			TYPEDEFS			*/
+	/********************************/
+			typedef				T		value_type;
+			typedef				Alloc	allocator_type;
+			typedef typename	allocator_type::reference		reference
+			typedef typename	allocator_type::const_reference	const_reference
+			typedef typename	allocator_type::pointer		pointer
+			typedef typename	allocator_type::const_pointer	const_pointer
+			typedef				ft::Random_access_iterator<value_type>	iterator;
+			typedef				ft::Random_access_iterator<const value_type>	const_iterator;
+			typedef				ft::Reverse_iterator<iterator>	reverse;
+			typedef				ft::Reverse_iterator<const_iterator>	const_reverse;
 
 			Vector();
 			Vector( Vector const & src );
@@ -33,8 +50,6 @@ template < typename T, class Alloc = std::allocator<T> >
 			typedef typename const_reverse_iterator
 			typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type	difference_type
 			typedef typename size_type
-
-		private:
 
 	};
 };
