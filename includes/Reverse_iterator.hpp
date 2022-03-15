43762ebdf259ef8294ba557e70dbcd8496f4f218
Elias AACHACH <elaachac@e2r4p18.clusters.42paris.fr>
2022-03-15 17:32:24 +0100

    reverse: operators wip, relationnal operators wip. == done

--- includes/Reverse_iterator.hpp
+++ includes/Reverse_iterator.hpp
@@ -50,8 +50,103 @@ namespace	ft
 	/********************************/
 	/*			OPERATORS			*/
 	/********************************/
-	
+
+		/*	Dereference Operators	*/
+		reference	operator*(void) const
+		{
+			Reverse_iterator	tmp = this->_current;
+			return(*--tmp);
+		}
+
+		/*	Pointer Operators	*/
+		pointer		operator->(void) const
+		{
+			return (&(operator*()));
+		}
+
+		/*	Incrementation Operators	*/
+		Reverse_iterator &	operator++(void)
+		{
+			this->_current--;
+			return (*this);
+		}
+		Reverse_iterator	operator++(int)
+		{
+			Reverse_iterator tmp = *this;
+			++(*this);
+			return (tmp);
+		}
+		Reverse_iterator &	operator--(void)
+		{
+			this->_current++;
+			return (*this);
+		}
+		Reverse_iterator	operator--(int)
+		{
+			Reverse_iterator tmp = *this;
+			--(*this);
+			return (tmp);
+		}
+
+		/*		Arithmetic Operators	*/
+
+		Reverse_iterator	operator+(diffrence_type n) const
+		{
+			return (Reverse_iterator(this->_current - n));
+		}
+		Reverse_iterator	operator-(diffrence_type n) const
+		{
+			return (Reverse_iterator(this->_current + n));
+		}
+
+		Reverse_iterator &	operator+=(diffrence_type n) const
+		{
+			this->_current -= n;
+			return (*this);
+		}
+		Reverse_iterator &	operator-=(diffrence_type n) const
+		{
+			this->_current += n;
+			return (*this);
+		}
+
+		reference	operator[]( difference_type n ) const
+		{
+			return *(*this + n);
+		}
+
+		operator reverse_iterator<const _Iterator> () const
+		{
+			return (reverse_iterator<const _Iterator>(this->_curr));
+		}
+
 	};
+	/********************************/
+	/*			OPERATORS			*/
+	/********************************/
+
+		/*		Arithmetic Operators	*/
+		template <typename T_L, typename T_R>
+		inline typename ft::Reverse_iterator<T_L>::difference_type
+		operator-(	const ft::Reverse_iterator<T_L> lhs, const ft::Reverse_iterator<T_R> rhs)
+		{
+			return ( lhs.base() - rhs.base() );
+		}
+		template<typename _Iterator>
+		inline Reverse_iterator<_Iterator>
+		operator+(	typename Reverse_iterator<_Iterator>::difference_type __n, const Reverse_iterator<_Iterator>& __x)
+		{
+			return reverse_iterator<_Iterator>(__x.base() - __n);
+		}
+
+		/*		Relational operators	*/
+		template<typename T_L, typename T_R>
+		inline bool		operator==(const ft::Reverse_iterator<T_L> lhs, const ft::Reverse_iterator<T_R> rhs)
+		{
+			return (lhs.base() == rhs.base());
+		}
+
+
 };
 
 #endif
