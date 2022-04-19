89382431ff6687c40b4b82e39a69bb656671dd12
Elias AACHACH <elaachac@e3r6p11.clusters.42paris.fr>
2022-04-19 18:18:41 +0200

    starting map

---
+++ includes/rbTree.hpp
@@ -0,0 +1,14 @@
+#ifndef RBTREE_HPP
+# define RBTREE_HPP
+
+#include "rbTreeNode.hpp"
+
+namespace ft
+{
+	class	rbTree
+	{
+		
+	}
+}
+
+#endif
---
+++ includes/rbTreeNode.hpp
@@ -0,0 +1,45 @@
+#ifndef RBTREENODE_HPP
+# define RBTREENODE_HPP
+
+# define RED 0
+# define BLACK 1
+
+namespace ft
+{
+	template <typename	T>
+	class node
+	{
+		public:
+
+		/********************************/
+		/*			TYPEDEFS			*/
+		/********************************/
+			typedef T	value_type;
+
+		protected:
+
+		/********************************/
+		/*		PRIVATE MEMBERS			*/
+		/********************************/
+			value_type	_value;
+			node		*_parent;
+			node		*_left;
+			node		*_right;
+			int			_color;
+		
+		public:
+		/********************************/
+		/*			CONSTRUCTOR			*/
+		/********************************/
+		/*default*/
+		node():	_value(T), _color(RED), _parent(NULL), _left(NULL), _right(NULL)
+		{
+		}
+		/*value*/
+		node(value_type value):	_value(value), _color(RED), _parent(NULL), _left(NULL), _right(NULL)
+		{
+		}
+	};
+};
+
+#endif
